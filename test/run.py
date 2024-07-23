9d0dacf326d4752de7f949761e37965a0a02d410
saimukzi <saimukzi@hiauntie.com>
2024-07-23 16:35:32 +0800

    start project, some test code

---
+++ test/run.py
@@ -0,0 +1,9 @@
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+
+env = Environment(
+    loader=FileSystemLoader("templates"),
+    autoescape=select_autoescape()
+)
+
+template = env.get_template("test.html")
+print(template.render())
