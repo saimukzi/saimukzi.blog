8a32b97ebe810f1ca0f67d73e2724dbec1ab40a4
西木子 <106793728+saimukzi@users.noreply.github.com>
2024-07-25 15:33:50 +0000

    change page to index, cmd_gen use runtime

--- cmd_gen.py
+++ cmd_gen.py
@@ -2,6 +2,8 @@ import argparse
 import jinja2
 import json
 import os
+import shutil
+import types
 
 import _common
 
@@ -11,25 +13,31 @@ def main():
 
     args = parser.parse_args()
 
+    runtime = types.SimpleNamespace()
+
     config_path = args.config
     with open(config_path, 'r') as f:
-        config_data = json.load(f)
+        runtime.config_data = json.load(f)
 
-    jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(config_data['templates_path']))
+    shutil.rmtree(runtime.config_data['output_path'], ignore_errors=True)
+
+    runtime.jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(runtime.config_data['templates_path']))
     
-    template_file_list = _common.find_file(config_data['templates_path'])
+    template_file_list = _common.find_file(runtime.config_data['templates_path'])
     for template_file in template_file_list:
-        rel_path = os.path.relpath(template_file, config_data['templates_path'])
-        output_file = os.path.join(config_data['output_path'], rel_path)
-        process_template(rel_path, output_file, config_data, jinja_env)
+        process_template(template_file, runtime)
 
-def process_template(rel_path, output_file, config_data, jinja_env):
-    template = jinja_env.get_template(rel_path)
+    # content_file_list = _common.find_file(runtime.config_data['content_path'])
+
+def process_template(template_file, runtime):
+    rel_path = os.path.relpath(template_file, runtime.config_data['templates_path'])
+    output_file = os.path.join(runtime.config_data['output_path'], rel_path)
+    template = runtime.jinja_env.get_template(rel_path)
     output_dir = os.path.dirname(output_file)
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
     render_data = {}
-    for k,v in config_data.items():
+    for k,v in runtime.config_data.items():
         render_data[f'config_{k}'] = v
     with open(output_file, 'w') as f:
         f.write(template.render(render_data))
