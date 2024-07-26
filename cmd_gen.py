467781abf3341187a8242d3eb39604f6e66db5b1
saimukzi <saimukzi@hiauntie.com>
2024-07-26 21:38:51 +0800

    content to article

--- cmd_gen.py
+++ cmd_gen.py
@@ -27,9 +27,9 @@ def main():
     for template_file in template_file_list:
         process_template(template_file, runtime)
 
-    content_file_list = _common.find_file(runtime.config_data['contents_path'])
-    for content_file in content_file_list:
-        process_content(content_file, runtime)
+    article_file_list = _common.find_file(runtime.config_data['articles_path'])
+    for article_file in article_file_list:
+        process_article(article_file, runtime)
 
 def process_template(template_file, runtime):
     rel_path = os.path.relpath(template_file, runtime.config_data['templates_path'])
@@ -47,12 +47,12 @@ def process_template(template_file, runtime):
 CONFIG_DATA_DEFAULT = {
     'enable': True,
 }
-def process_content(content_file, runtime):
-    content = _common.read_file(content_file)
+def process_article(article_file, runtime):
+    article = _common.read_file(article_file)
 
-    config_start_line_num = content.index('=== CONFIG START ===')
-    config_end_line_num = content.index('=== CONFIG END ===')
-    config_lines = content[config_start_line_num+1:config_end_line_num]
+    config_start_line_num = article.index('=== CONFIG START ===')
+    config_end_line_num = article.index('=== CONFIG END ===')
+    config_lines = article[config_start_line_num+1:config_end_line_num]
     config_data = '\n'.join(config_lines)
     config_data = json.loads(config_data)
     config_data = {**CONFIG_DATA_DEFAULT, **config_data}
@@ -60,10 +60,10 @@ def process_content(content_file, runtime):
     if not config_data['enable']:
         return
     
-    content_start_line_num = content.index('=== CONTENT START ===')
-    content_end_line_num = content.index('=== CONTENT END ===')
-    content_lines = content[content_start_line_num+1:content_end_line_num]
-    content = '\n'.join(content_lines)
+    article_start_line_num = article.index('=== ARTICLE START ===')
+    article_end_line_num = article.index('=== ARTICLE END ===')
+    article_lines = article[article_start_line_num+1:article_end_line_num]
+    article = '\n'.join(article_lines)
 
     id_hash = _common.md5(config_data['id'])
     output_folder_path = os.path.join(runtime.config_data['output_path'], id_hash[:2], id_hash[2:4], id_hash)
@@ -73,9 +73,9 @@ def process_content(content_file, runtime):
     with open(meta_output_path, 'w') as f:
         json.dump(config_data, f, indent=2, sort_keys=True)
     
-    content_output_path = os.path.join(output_folder_path, 'content.txt')
-    with open(content_output_path, 'w') as f:
-        f.write(content)
+    article_output_path = os.path.join(output_folder_path, 'article.txt')
+    with open(article_output_path, 'wt', encoding='utf-8') as f:
+        f.write(article)
 
 
 if __name__ == '__main__':
