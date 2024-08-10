55cc2a758c666e2a3903531c8f92ab942e730240
saimukzi <saimukzi@hiauntie.com>
2024-08-10 12:52:10 +0800

    tag_list to tag_data_list

--- _feature_tags.py
+++ _feature_tags.py
@@ -21,16 +21,22 @@ def _step_tag_id_to_data_dict_ready(runtime):
 
 _STEP_DEPENDENCY_LIST.append((_feature_articles._step_article_meta_list_ready, _step_gen_tag_id_to_data_dict, _step_tag_id_to_data_dict_ready))
 
-def _step_meta_tag_list(runtime):
-    tag_list = runtime.article_meta_list
-    tag_list = map(lambda x: x['tags'], tag_list)
-    tag_list = sum(tag_list, [])
-    tag_list = list(sorted(set(tag_list)))
-    runtime.blog_meta_dict['tag_list'] = tag_list
+def _step_meta_tag_data_list(runtime):
+    tag_data_list = runtime.article_meta_list
+    tag_data_list = map(lambda x: x['tags'], tag_data_list)
+    tag_data_list = sum(tag_data_list, [])
+    value_to_count_dict = {}
+    for tag in tag_data_list:
+        value_to_count_dict[tag] = value_to_count_dict.get(tag, 0) + 1
+    tag_data_list = set(tag_data_list)
+    tag_data_list = map(lambda x: {'tag_id': x, 'count': value_to_count_dict[x]}, tag_data_list)
+    tag_data_list = sorted(tag_data_list, key=lambda x: (-x['count'], x['tag_id']))
+    tag_data_list = list(tag_data_list)
+    runtime.blog_meta_dict['tag_data_list'] = tag_data_list
 
-_STEP_DEPENDENCY_LIST.append((_feature_base._step_init_done, _step_meta_tag_list))
-_STEP_DEPENDENCY_LIST.append((_feature_articles._step_article_meta_list_ready, _step_meta_tag_list))
-_STEP_DEPENDENCY_LIST.append((_step_meta_tag_list, _feature_base._step_output_ready))
+_STEP_DEPENDENCY_LIST.append((_feature_base._step_init_done, _step_meta_tag_data_list))
+_STEP_DEPENDENCY_LIST.append((_feature_articles._step_article_meta_list_ready, _step_meta_tag_data_list))
+_STEP_DEPENDENCY_LIST.append((_step_meta_tag_data_list, _feature_base._step_output_ready))
 
 def _step_output(runtime):
     os.makedirs(os.path.join(runtime.config_data['output_path'], 'tags'), exist_ok=True)
